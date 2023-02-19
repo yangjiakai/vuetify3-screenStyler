7787f0b4c4731a0fdf706e0052886225dd7ffb9c
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-02-19 23:34:28 +0900

    Update AppBar.vue

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -10,7 +10,7 @@ const customizeTheme = useCustomizeThemeStore();
 </script>
 
 <template>
-  <v-app-bar elevation="1" location="bottom">
+  <v-app-bar elevation="1" location="top">
     <template v-slot:prepend>
       <transition>
         <v-icon
